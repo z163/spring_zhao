0d9d92ace19407702b159a3e0987bb42f832a601
Zhen Wang <zhenw2676@163.com>
2019-07-01 20:27:46 -0700

    spring static factory

--- spring_bean_zhao/src/main/java/com/kaikeba/util/TeacherFactory.java
+++ spring_bean_zhao/src/main/java/com/kaikeba/util/TeacherFactory.java
@@ -3,8 +3,8 @@ package com.kaikeba.util;
 import com.kaikeba.beans.Teacher;
 
 public class TeacherFactory {
-
-	  public Teacher createTeacher(){
+      //节省内存消耗
+	  public static Teacher createTeacher(){
 		      Teacher teacher =    new Teacher();
 		      System.out.println("TeacherFactory 负责创建 teacher类实例对象..");
 		      return teacher;
